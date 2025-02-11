013945dcf3d40dcadc102ac8af6518506d070c72
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-02-11 15:24:39 +0500

    view new, old requests, polish up everything

--- src/main/java/com/CRMSystem/CRMSystem/Controller.java
+++ src/main/java/com/CRMSystem/CRMSystem/Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -59,6 +60,53 @@ public class Controller {
         model.addAttribute("applicationRequest", applicationRequest);
 
         return "detail";
+    }
+
+    @PostMapping("/detail/{id}")
+    public String edit(@PathVariable Long id, Model model, @RequestParam("action") String action) {
+
+        ApplicationRequest applicationRequest = applicationRepository.findById(id).orElse(null);
+
+        if(action.equals("obrabotka")) {
+            assert applicationRequest != null;
+            applicationRequest.setHandled(true);
+            applicationRepository.save(applicationRequest);
+            return "redirect:/detail/" + id;
+        }
+        else{
+            assert applicationRequest != null;
+            applicationRepository.delete(applicationRequest);
+        }
+
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/new")
+    public String newApplication(Model model) {
+        List<ApplicationRequest> allApplications = applicationRepository.findAll();
+
+        List<ApplicationRequest>  newApplications = allApplications.stream().filter(
+                a ->!a.isHandled()
+        ).collect(Collectors.toList());
+
+        model.addAttribute("applicationsNew", newApplications);
+
+        return "new";
+
+    }
+
+    @GetMapping("/old")
+    public String oldApplication(Model model) {
+        List<ApplicationRequest> allApplications = applicationRepository.findAll();
+
+        List<ApplicationRequest> oldApplications = allApplications.stream()
+                .filter(a -> a.isHandled())
+                .collect(Collectors.toList());
+
+        model.addAttribute("applicationsOld", oldApplications);
+
+        return "old";
     }
 
 }
